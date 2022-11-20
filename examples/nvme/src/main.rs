abce249fb59213c66d37fe9f98386160264edeb0
espoal <alberto.websci@gmail.com>
2022-11-20 21:46:48 +0100

    feat: now compiles

--- examples/nvme/src/main.rs
+++ examples/nvme/src/main.rs
@@ -1,27 +1,28 @@
 mod nvme;
 
-use std::os::unix::fs;
-use std::os::unix::io::{AsRawFd};
-use std::os::unix::io;
+use std::os::unix::io::AsRawFd;
+use std::{fs, io};
 
 use io_uring::squeue::Entry;
 use io_uring::types::Fd;
-use io_uring::{opcode, types, IoUring};
+use io_uring::{cqueue, opcode, squeue, IoUring};
 
+use crate::nvme::nvme_uring_cmd_io;
 use nvme::nvme_uring_cmd;
 
-fn main() -> io::Result<()> {
+// Double check output with:
+// sudo nvme read /dev/nvme0n1 -s 1000000 -c 1 -z 4096
 
+fn main() -> io::Result<()> {
     // Params
-    let path = "/dev/nvme0";
-    let lba: u64 = 1000;
+    let path = "/dev/nvme0n1";
+    let lba: u64 = 1000000;
     let num_blocks: u32 = 1;
 
-    // TODO: find nsid
+    // sudo nvme id-ns /dev/nvme0n1
     let nsid = 1;
 
-
-    let mut builder = IoUring::builder();
+    let mut builder = IoUring::<squeue::Entry128, cqueue::Entry32>::generic_builder();
     let mut ring = builder.build(128)?;
 
     let fd = fs::File::open(path)?;
@@ -30,16 +31,14 @@ fn main() -> io::Result<()> {
     let mut buf: *mut u8 = &mut buff[0];
     let tfd = Fd(fd.as_raw_fd());
 
-
     // TODO: check correct cmd_opcode
-    let cmd_op: u32 = 0x80;
-    let opcode: u8 = 0x2;
+    let cmd_op = nvme_uring_cmd_io() as u32;
+    let opcode = 0x02 as u8;
     let data_addr = buf as u64;
     let data_len = 1 as u32;
     let cdw10 = (lba & 0xffffffff) as u32;
     let cdw11 = (lba >> 32) as u32;
     let cdw12 = num_blocks - 1;
-
 
     let cmd = nvme_uring_cmd {
         opcode,
@@ -76,11 +75,16 @@ fn main() -> io::Result<()> {
 
     let cqe = ring.completion().next().expect("completion queue is empty");
 
-    assert_eq!(cqe.user_data(), 0x42);
-    assert!(cqe.result() >= 0, "read error: {}", cqe.result());
+    println!("cmd bytes: {:?}", cmd_bytes);
+    println!("cqe: {:?}", cqe);
+    println!("cmd: {:?}", nvme_read);
+
+    assert_eq!(cqe.user_data(), 0x22);
+    //assert!(cqe.result() >= 0, "read error: {}", cqe.result());
+    println!("read {} bytes", cqe.result());
 
     let content = std::str::from_utf8(&buff).unwrap();
-    println!("bytes read: {:?}", content);
+    //println!("bytes read: {:?}", content);
 
     Ok(())
 }
--- examples/nvme/src/nvme.rs
+++ examples/nvme/src/nvme.rs
@@ -3,10 +3,10 @@
 // 2. Is buff done correctly?
 // 3. Does it looks right?
 
-pub type __u8 = core::std::os::raw::c_uchar;
-pub type __u16 = core::std::os::raw::c_ushort;
-pub type __u32 = core::std::os::raw::c_uint;
-pub type __u64 = core::std::os::raw::c_ulonglong;
+pub type __u8 = std::os::raw::c_uchar;
+pub type __u16 = std::os::raw::c_ushort;
+pub type __u32 = std::os::raw::c_uint;
+pub type __u64 = std::os::raw::c_ulonglong;
 
 #[repr(C)]
 #[derive(Debug, Default, Copy, Clone)]
@@ -29,4 +29,27 @@ pub struct nvme_uring_cmd {
     pub cdw15: __u32,
     pub timeout_ms: __u32,
     pub rsvd2: __u32,
+}
+
+const IOC_NR_BITS: usize = 8;
+const IOC_TYPE_BITS: usize = 8;
+const IOC_SIZE_BITS: usize = 14;
+const IOC_NR_SHIFT: usize = 0;
+const IOC_READ: usize = 2;
+const IOC_WRITE: usize = 2;
+
+const IOC_TYPE_SHIFT: usize = IOC_NR_SHIFT + IOC_NR_BITS;
+const IOC_SIZE_SHIFT: usize = IOC_TYPE_SHIFT + IOC_TYPE_BITS;
+const IOC_DIRSHIFT: usize = IOC_SIZE_SHIFT + IOC_SIZE_BITS;
+
+fn ioc(dir: usize, t: usize, nr: usize, size: usize) -> usize {
+    (dir << IOC_DIRSHIFT) | (t << IOC_TYPE_SHIFT) | (nr << IOC_NR_SHIFT) | (size << IOC_SIZE_SHIFT)
+}
+
+fn iowr(t: usize, nr: usize, size: usize) -> usize {
+    ioc(IOC_READ | IOC_WRITE, t, nr, size)
+}
+
+pub fn nvme_uring_cmd_io() -> u32 {
+    iowr('N' as usize, 0x80, core::mem::size_of::<nvme_uring_cmd>()) as u32
 }
